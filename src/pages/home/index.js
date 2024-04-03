47327bafd08cc78b49e9ed2c286aff06d87c2065
João Marcelo <tobler.joao.marcelo@gmail.com>
2024-04-03 15:39:22 -0300

    primeiro commit

---
+++ src/pages/home/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+
+export default function Home(){
+    return (
+        <> 
+        <div className = 'Welcome'>
+            <h1>Bem vindo ao Sistema!</h1>
+            <h2>Navegue pelo menu acima para acessar as areas de cadastro e as listas</h2>
+            <h5>Para utilizar o sistema é preciso logar!</h5>
+        </div>
+        
+        <footer>
+            <p>&copy; 2024 Minha Empresa. Todos os direitos reservados.</p>
+        </footer>
+        </>
+    )
+}
